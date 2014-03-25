b2813d8a0624dc24e3aff296a91e17d090764a9a
kevit <simarg@gmail.com>
2014-03-25 23:38:46 +0300

    add databags

--- recipes/gitdeploy.rb
+++ recipes/gitdeploy.rb
@@ -10,20 +10,30 @@ directory "/opt/repos" do
 end
 
 
-git "/opt/repos/testgem" do
-   repository "https://github.com/kevit/testgem.git"
-   revision "HEAD"
-   action :sync
-   notifies :run, "bash[upload_gem]"
-end
+gems = data_bag('gems')
 
- bash "upload_gem" do
-   cwd "/opt/repos/testgem"
+gems.each do |gem_info|
+  current_gem = data_bag_item('gems', gem_info)
+  gem_name = current_gem['id']
+  gem_version = current_gem['version']
+  git "/opt/repos/#{gem_name}" do
+     repository current_gem['repository_url']
+     revision current_gem['revision']
+     action :sync
+     notifies :run, "bash[upload_gem]"
+  end
+
+  bash "upload_gem" do
+   cwd "/opt/repos/#{gem_name}"
    code <<-EOH
-   gem build testgem.gemspec
+   gem build #{gem_name}.gemspec
    gem sources --add http://127.0.0.1:9010
-   gem inabox -o -g http://127.0.0.1:9010 testgem-0.0.1.gem
+   gem inabox -o -g http://127.0.0.1:9010 testgem-#{gem_version}.gem
    EOH
    environment 'PREFIX' => "/usr/local"
    action :nothing
- end
+  end
+
+end
+
+
---
+++ test/integration/default/serverspec/geminabox_daemon_spec.rb
@@ -0,0 +1,23 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+    c.before :all do
+          c.path = '/sbin:/usr/sbin'
+            end
+end
+
+describe "geminabox service" do
+
+    it "is listening on port 9010" do
+          expect(port(9010)).to be_listening
+    end
+
+    it "has a running service geminabox " do
+          expect(service("geminabox")).to be_running
+    end
+
+end
+
